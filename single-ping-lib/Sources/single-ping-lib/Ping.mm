0007402d73cd3f3c06d264bfe014c87d021baa16
kukushechkin <kukushechkin@mac.com>
2021-09-16 22:51:14 +0300

    milliseconds, right

--- single-ping-lib/Sources/single-ping-lib/Ping.mm
+++ single-ping-lib/Sources/single-ping-lib/Ping.mm
@@ -6,5 +6,6 @@ double Ping::latencyForDestination(const std::string& dest) {
     // TODO: stringWithUTF8String: will copy string, not great, this also means move semantics make no difference
     NSString * destination = [NSString stringWithUTF8String:dest.c_str()];
     SimplePingObj* simplePingObj = [SimplePingObj new];
-    return [simplePingObj singlePingWithHostName:destination];
+    NSTimeInterval latency = [simplePingObj singlePingWithHostName:destination];
+    return latency == INFINITY ? latency : latency * 1000.0;
 }
